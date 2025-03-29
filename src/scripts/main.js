e9725982f9d7e69883704d3d8cc9bcd9e8c0a8cb
Daryna Kukharets <darinakuharec61@gmail.com>
2025-03-30 01:11:00 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { animation } from './components/animation.js';
-import { slider } from './components/slider.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  slider();
-  animation();
-});
